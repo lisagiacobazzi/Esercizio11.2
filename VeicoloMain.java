8fa69086c032a4b847d095636ee4befdaf225de1
lisagiacobazzi <71656222+lisagiacobazzi@users.noreply.github.com>
2020-10-11 12:42:50 +0200

    Create VeicoloMain.java

---
+++ VeicoloMain.java
@@ -0,0 +1,17 @@
+public class VeicoloMain{
+	public static void main(String[] args) {
+		Automobile a;
+		Bicicletta b;
+		
+		a = new Automobile ("BL987CT");
+		b = new Bicicletta ();
+		
+		a.accellera(3.5,4);
+		b.pedala(8,3);
+		
+		System.out.print("La velocità della macchina è: ");
+		a.printVelocita(a);
+		System.out.print("La velocità della bicicletta è: ");
+		b.printVelocita(b);
+		}
+}
